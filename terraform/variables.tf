dc6fe142b9457ed22688936fdd27fc248e2209ea
artivis <jeremie.deray@canonical.com>
2025-06-06 12:02:04 +0200

    add terraform
    Signed-off-by: artivis <jeremie.deray@canonical.com>

---
+++ terraform/variables.tf
@@ -0,0 +1,60 @@
+variable "app_name" {
+  description = "Application name"
+  type        = string
+  nullable    = false
+  default     = "cos-registration-server"
+}
+
+variable "channel" {
+  description = "Charm channel"
+  type        = string
+  nullable    = false
+  default     = "latest/edge"
+}
+
+variable "config" {
+  description = "Config options as in the ones we pass in juju config"
+  type        = map(string)
+  default     = {}
+}
+
+# We use constraints to set AntiAffinity in K8s
+# https://discourse.charmhub.io/t/pod-priority-and-affinity-in-juju-charms/4091/13?u=jose
+variable "constraints" {
+  description = "Constraints to be applied"
+  type        = string
+  default     = ""
+}
+
+variable "model" {
+  description = "Model name (must be a machine model)"
+  type        = string
+  nullable    = false
+}
+
+variable "revision" {
+  description = "Charm revision"
+  type        = number
+  nullable    = true
+  default     = null
+}
+
+variable "units" {
+  description = "Number of units"
+  type        = number
+  default     = 1
+}
+
+variable "resources" {
+  description = "Resources used by the charm"
+  type        = map(string)
+  default     = {
+    cos-registration-server-image: "ghcr.io/ubuntu-robotics/cos-registration-server:dev"
+  }
+}
+
+variable "storage" {
+  description = "Map of storage used by the application. Defaults to 1 GB, allocated by Juju"
+  type        = map(string)
+  default     = {}
+}

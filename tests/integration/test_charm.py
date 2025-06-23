ace2447655b1e80b2e0e0633ef7030d9ba7171e7
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2025-06-23 16:51:09 +0200

    fix: fmt

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -206,9 +206,7 @@ async def test_tracing(ops_test: OpsTest):
 async def test_integrate_blackbox(ops_test: OpsTest):
     # @todo: upgrade to stable when blackbox charm with probes relation
     # is promoted from edge.
-    await ops_test.model.deploy(
-        "blackbox-exporter-k8s", "blackbox", channel="1/edge", trust=True
-    )
+    await ops_test.model.deploy("blackbox-exporter-k8s", "blackbox", channel="1/edge", trust=True)
 
     logger.info(
         "Adding relation: %s:%s",
